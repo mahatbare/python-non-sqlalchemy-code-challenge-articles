db5bfb3af83a65e37402f9a4ded31be33e085ed8
mahatbare <mahatbare1240@gmail.com>
2024-07-16 17:31:58 +0300

    code-challenge-lab-complete

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,177 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+# class Article:
+#     def __init__(self, author, magazine, title):
+#         self.author = author
+#         self.magazine = magazine
+#         self.title = title
         
+#         def __init__(self, author):
+#             self.author = author    
+        
+#         def __init__(self, magazine):
+#             self.magazine = magazine  
+            
+#         def __init__(self, title):
+#             self.title = title         
+        
+        
+# class Author:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def articles(self):
+#         pass
+
+#     def magazines(self):
+#         pass
+
+#     def add_article(self, magazine, title):
+#         pass
+
+#     def topic_areas(self):
+#         pass
+
+# class Magazine:
+#     def __init__(self, name, category):
+#         self.name = name
+#         self.category = category
+
+#     def articles(self):
+#         pass
+
+#     def contributors(self):
+#         pass
+
+#     def article_titles(self):
+#         pass
+
+#     def contributing_authors(self):
+#         pass
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+        else:
+            raise ValueError("Name must be a non-empty string")
+
+    @property
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        categories = list(set([magazine.category for magazine in self.magazines()]))
+        return categories if categories else None
 
 class Magazine:
+    all_magazines = []
+
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if isinstance(name, str) and 2 <= len(name) <= 16:
+            self._name = name
+        else:
+            raise ValueError("Name must be a string between 2 and 16 characters")
+        
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category
+        else:
+            raise ValueError("Category must be a non-empty string")
+        
+        Magazine.all_magazines.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 2 and 16 characters")
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+        else:
+            raise ValueError("Category must be a non-empty string")
 
     def articles(self):
-        pass
+        return [article for article in Article.all_articles if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        authors = [article.author for article in self.articles()]
+        contributing_authors = [author for author in set(authors) if authors.count(author) > 2]
+        return contributing_authors if contributing_authors else None
+
+    @classmethod
+    def top_publisher(cls):
+        top_magazine = max(cls.all_magazines, key=lambda mag: len(mag.articles()), default=None)
+        return top_magazine
+
+class Article:
+    all_articles = []
+
+    def __init__(self, author, magazine, title):
+        if isinstance(author, Author):
+            self._author = author
+        else:
+            raise ValueError("Author must be of type Author")
+        
+        if isinstance(magazine, Magazine):
+            self._magazine = magazine
+        else:
+            raise ValueError("Magazine must be of type Magazine")
+        
+        if isinstance(title, str) and 5 <= len(title) <= 50:
+            self._title = title
+        else:
+            raise ValueError("Title must be a string between 5 and 50 characters")
+
+        Article.all_articles.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+            self._author = value
+        else:
+            raise ValueError("Author must be of type Author")
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if isinstance(value, Magazine):
+            self._magazine = value
+        else:
+            raise ValueError("Magazine must be of type Magazine")
--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import ipdb;
+import ipdb ;
 
 from classes.many_to_many import Article
 from classes.many_to_many import Author
@@ -11,3 +11,37 @@ if __name__ == '__main__':
 
     # don't remove this line, it's for debugging!
     ipdb.set_trace()
+
+# lib/debug.py
+
+# from  owner_pet import Author, Magazine, Article
+
+# Create authors
+author1 = Author("John Doe")
+author2 = Author("Jane Smith")
+
+# Create magazines
+mag1 = Magazine("Tech Today", "Technology")
+mag2 = Magazine("Health Weekly", "Health")
+
+# Create articles
+article1 = Article(author1, mag1, "The Future of AI")
+article2 = Article(author1, mag2, "Healthy Living Tips")
+article3 = Article(author2, mag1, "Tech Innovations")
+
+# Test Author methods
+print(author1.articles())  # Should return [article1, article2]
+print(author1.magazines())  # Should return [mag1, mag2]
+print(author1.topic_areas())  # Should return ["Technology", "Health"]
+
+# Test Magazine methods
+print(mag1.articles())  # Should return [article1, article3]
+print(mag1.contributors())  # Should return [author1, author2]
+print(mag1.article_titles())  # Should return ["The Future of AI", "Tech Innovations"]
+print(mag1.contributing_authors())  # Should return []
+
+# Run the script
+if __name__ == "__main__":
+    import pytest
+    pytest.main()
+
